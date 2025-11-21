581e3144c149b7b67386b8bc14abf036bb07a765
Danikim01 <dakim@fi.uba.ar>
2025-11-21 20:57:49 -0300

    send new msg uuid sin sharding router, when sending flushing batches

--- src/workers/sharding/sharding_router.py
+++ src/workers/sharding/sharding_router.py
@@ -118,7 +118,7 @@ class ShardingRouter(BaseWorker):
                 client_id,
                 batch,
                 routing_key=routing_key,
-                # message_uuid=str(uuid.uuid4()),
+                message_uuid=str(uuid.uuid4()),
             )
             
             # Update statistics
